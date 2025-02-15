a8318cb9cc64e4b4f77d590f1fed13b11f099f6a
Timirlon <tturgimbayev@gmail.com>
2025-02-15 11:38:08 +0500

    upd movie

--- src/main/java/org/example/springjpa/controller/MovieController.java
+++ src/main/java/org/example/springjpa/controller/MovieController.java
@@ -6,6 +6,7 @@ import org.example.springjpa.repository.MovieRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,12 +49,60 @@ public class MovieController {
     }
 
     @GetMapping("/filter-by-rating-year")
-    public List<Movie> findAllByRatingBetweenAndYearBetween(@RequestParam double minRating, @RequestParam double maxRating, @RequestParam int minYear, @RequestParam int maxYear) {
-        return movieRepository.findAllByRatingBetweenAndReleaseYearBetween(minRating, maxRating, minYear, maxYear);
+    public List<Movie> findAllByRatingBetweenAndYearBetween(@RequestParam double minRating, @RequestParam double maxRating, @RequestParam int startYear, @RequestParam int endYear) {
+        return movieRepository.findAllByRatingBetweenAndReleaseYearBetween(minRating, maxRating, startYear, endYear);
     }
 
     @GetMapping("/filter-by-years-in")
     public List<Movie> findAllByReleaseYearIn(@RequestParam List<Integer> years) {
         return movieRepository.findAllByReleaseYearIn(years);
+    }
+
+    @GetMapping("/filter")
+    public List<Movie> findAllByRatingBetweenAndReleaseYearBetweenSortedAndPaged(
+            @RequestParam double minRating, @RequestParam double maxRating,
+            @RequestParam int startYear, @RequestParam int endYear,
+            @RequestParam String sortBy, @RequestParam String sortOrder,
+            @RequestParam int page, @RequestParam int size
+    ) {
+        if (page < 1 || size < 1 || size > 50) {
+            throw new IndexOutOfBoundsException();
+        }
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+
+        Sort.Direction dir = Sort.Direction.ASC;
+        if (sortOrder.equals("desc")) {
+            dir = Sort.Direction.DESC;
+        }
+
+        return movieRepository.findAllByRatingBetweenAndReleaseYearBetween(
+                minRating, maxRating,
+                startYear, endYear,
+                Sort.by(dir, sortBy), pageable);
+    }
+
+    @GetMapping("/search")
+    public List<Movie> findAllByTitleContainingAndRatingGreaterThanAndReleaseYearAfterSortedAndPaged(
+            @RequestParam String query,
+            @RequestParam double minRating,
+            @RequestParam int startYear,
+            @RequestParam String sortBy, @RequestParam String sortOrder,
+            @RequestParam int page, @RequestParam int size
+    ) {
+        if (page < 1 || size < 1 || size > 50) {
+            throw new IndexOutOfBoundsException();
+        }
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+
+        Sort.Direction dir = Sort.Direction.ASC;
+        if (sortOrder.equals("desc")) {
+            dir = Sort.Direction.DESC;
+        }
+
+        return movieRepository.findAllByTitleContainingIgnoreCaseAndRatingGreaterThanAndReleaseYearAfter(
+                query, minRating, startYear,
+                Sort.by(dir, sortBy), pageable);
     }
 }
--- src/main/java/org/example/springjpa/repository/MovieRepository.java
+++ src/main/java/org/example/springjpa/repository/MovieRepository.java
@@ -3,6 +3,7 @@ package org.example.springjpa.repository;
 import org.example.springjpa.model.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
@@ -11,7 +12,21 @@ import java.util.List;
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
     Page<Movie> findAllByRatingBetween(double minRating, double maxRating, Pageable pageable);
 
-    List<Movie> findAllByRatingBetweenAndReleaseYearBetween(double minRating, double maxRating, int minYear, int maxYear);
+    List<Movie> findAllByRatingBetweenAndReleaseYearBetween(
+            double minRating, double maxRating,
+            int startYear, int endYear);
 
     List<Movie> findAllByReleaseYearIn(Collection<Integer> releaseYearCol);
+
+    List<Movie> findAllByRatingBetweenAndReleaseYearBetween(
+            double minRating, double maxRating,
+            int startYear, int endYear,
+            Sort sort, Pageable pageable);
+
+    List<Movie> findAllByTitleContainingIgnoreCaseAndRatingGreaterThanAndReleaseYearAfter(
+            String subStr,
+            double minRating,
+            int startYear,
+            Sort sort,
+            Pageable pageable);
 }
